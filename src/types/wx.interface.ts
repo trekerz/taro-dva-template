d1fa0547d4c1b12645e25c0408af413cb7c3d790
zhuangjiafeng <zhuangjiafeng@meizhitec.com>
2020-01-08 16:48:36 +0800

    微信api数据类型

---
+++ src/types/wx.interface.ts
@@ -0,0 +1,12 @@
+/**
+ * 用户信息数据类型
+ */
+export interface IWXUserInfo {
+  nickName: string
+  avatarUrl: string
+  gender: number
+  country: string
+  province: string
+  city: string
+  language: string
+}
